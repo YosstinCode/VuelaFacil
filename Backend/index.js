4b4322032063f5cb80ec06ee75913a399ee40aad
YosstinCode <Yosstincode@gmail.com>
2021-11-06 10:55:01 -0500

    connection to firebase with service account

--- Backend/index.js
+++ Backend/index.js
@@ -1,30 +1,76 @@
-const { initializeApp } = require("firebase/app");
+const admin = require("firebase-admin");
 
-const { getFirestore, collection, addDoc } = require("firebase/firestore");
+const serviceAccount = require("./vuela-facil-grupo-10-d2c6f44ad686.json");
 
-const firebaseConfig = {
-  apiKey: "AIzaSyAkuWVewppCfqMSNZps7_NPjgku546BIGk",
-  authDomain: "vuela-facil-grupo-10.firebaseapp.com",
-  projectId: "vuela-facil-grupo-10",
-  storageBucket: "vuela-facil-grupo-10.appspot.com",
-  messagingSenderId: "712616377986",
-  appId: "1:712616377986:web:e2971c60fd2206ea06fe9b",
-  measurementId: "G-0GN3TQW78Z",
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+});
+
+const db = admin.firestore();
+
+const createNewEntity = (collectionName, data) => {
+  if (typeof data !== "object") throw new Error("type of data not is correct");
+
+  db.collection(collectionName)
+    .doc()
+    .set(data)
+    .then((res) => console.log(res))
+    .catch((e) => console.error("Error adding document: ", e));
 };
 
-const appInitialized = initializeApp(firebaseConfig);
+// for to add a users in firestore
 
-const db = getFirestore();
+createNewEntity("users", {
+  name: "yosstin",
+  user_name: "yosstinCode",
+  terms_and_conditions: true,
+  creation_date: new Date(),
+});
 
-try {
-  const docRef = async () =>
-    await addDoc(collection(db, "users"), {
-      firstName: "Nataly",
-      lastName: "Carvajal",
-      born: 2002,
-    });
+// for to add a agency in firestore
 
-  console.log("Document written with ID: ");
-} catch (e) {
-  console.error("Error adding document: ", e);
-}
+createNewEntity("agency", {
+  name: "Avianca",
+  description: "Esta es una descripcion de la empresa",
+  phone_number: "123456",
+  terms_and_conditions: true,
+  creation_date: new Date(),
+});
+
+// for to add a route in firestore
+
+createNewEntity("route", {
+  route_code: "A15",
+  destination_city: "Mexico",
+  origin_city: "Colombia",
+  price: 1_000_000,
+  departure_date: new Date("2021-12-13"),
+  return_date: new Date("2021-12-20"),
+  discount: 0.5,
+  plane: [
+    {
+      plane_code: "12",
+      seats_economyc_class: 20,
+      seats_business_class: 20,
+      seats_first_class: 20,
+    },
+  ],
+});
+
+/*++++++++++++++++++++++++++++++++++*/
+
+//for delete a users in firestore
+
+// db.collection("users")
+//   .doc("aMNdfNgspfnLdvka1OHr") //-> name or id of document in collection users
+//   .delete()
+//   .then((res) => console.log(res))
+//   .catch((e) => console.error("Error removing document: ", e));
+
+//for delete a agency in firestore
+
+// db.collection("agency")
+//   .doc("aMNdfNgspfnLdvka1OHr") //-> name or id of document in collection users
+//   .delete()
+//   .then((res) => console.log(res))
+//   .catch((e) => console.error("Error removing document: ", e));
